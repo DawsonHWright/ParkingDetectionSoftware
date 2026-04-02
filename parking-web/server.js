7fd8d0280d5fc7ef41c3645148602035b0ec70a5
DawsonHWright <dawsonhunterwright@gmail.com>
2026-04-02 06:00:26 -0600

    updating for cloud

--- parking-web/server.js
+++ parking-web/server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
-app.use(cors());
+
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 const DATA_DIR = path.join(__dirname, "data");
 const STATUS_FILE = path.join(DATA_DIR, "status.json");
